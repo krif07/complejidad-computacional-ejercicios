9e10df93e033c085e90b385438336f5776d8f2ae
Cristian Davila <krif07@gmail.com>
2018-08-06 11:40:35 -0500

    Commit inicial

---
+++ src/com/co/cbg/cp/ejercicios1/Ejercicios.java
@@ -0,0 +1,26 @@
+package com.co.cbg.cp.ejercicios1;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/*1) Hacer una función que reciba un número entero positivo y retorne 
+	verdadero si es primo o falso sino.*/
+	
+	/*2) Hacer una función que reciba un valor N y retorne la sumatoria de los
+	enteros hasta N.*/
+	
+	/*Hacer una función que llene un vector (de tamaño N) con números
+	enteros aleatorios (puede ser de 4 cifras).*/
+	
+	/*Hacer una función que llene una matriz de tamaño NxM con números
+	aleatorios.*/
+	
+	/*Hacer una función que reciba 2 matrices de tamaño NxM y MxP
+	respectivamente y devuelva otra matriz con la multiplicación de las dos	primeras*/
+	
+
+}
