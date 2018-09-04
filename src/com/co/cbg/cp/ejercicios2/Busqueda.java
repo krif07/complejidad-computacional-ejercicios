6a0e04b096d0072b27be0ac758c25eea5f3d1982
Cristian F. Dávila <krif07@gmail.com>
2018-09-03 21:33:03 -0500

    Se arregla error de método de búsqueda binario

--- src/com/co/cbg/cp/ejercicios2/Busqueda.java
+++ src/com/co/cbg/cp/ejercicios2/Busqueda.java
@@ -3,60 +3,100 @@ package com.co.cbg.cp.ejercicios2;
 public class Busqueda {
 
 	public DatosEstadisticos secuencial(long elemento, long[] datos) {
-		
+
 		int n = datos.length;
-		
+
 		DatosEstadisticos datosEstadisticos = new DatosEstadisticos();
 		datosEstadisticos.setTamVector(n);
-		
-		for(int i=0; i<n; i++) {
-			
+
+		for (int i = 0; i < n; i++) {
+
 			datosEstadisticos.incrementarComparaciones();
-			if(elemento==datos[i]) {
+			if (elemento == datos[i]) {
 				datosEstadisticos.setDatoEncontrado(true);
 				datosEstadisticos.imprimirEstadisticos();
 				return datosEstadisticos;
 			}
 		}
-		
+
 		datosEstadisticos.setDatoEncontrado(false);
 		datosEstadisticos.imprimirEstadisticos();
-		
+
 		return datosEstadisticos;
-		
+
 	}
-	
+
 	public DatosEstadisticos binaria(long elemento, long[] datos) {
-		
+
 		int limiteInferior = 0;
 		int limiteSuperior = datos.length;
 		int medio = 0;
-		
+
 		DatosEstadisticos datosEstadisticos = new DatosEstadisticos();
 		datosEstadisticos.setTamVector(limiteSuperior);
-			
-		do {	
-			
+
+		do {
+
 			medio = (limiteSuperior - limiteInferior) / 2;
-			
+
 			datosEstadisticos.incrementarComparaciones();
-			if(elemento==datos[medio]) {
-				//datosEstadisticos.incrementarComparaciones();
+
+			if (elemento == datos[medio]) {
+				// datosEstadisticos.incrementarComparaciones();
 				datosEstadisticos.setDatoEncontrado(true);
 				datosEstadisticos.imprimirEstadisticos();
+
+				System.out.println("---------------------elementoE " + elemento);
 				return datosEstadisticos;
+			} else if (elemento > datos[medio]) {
+				// datosEstadisticos.incrementarComparaciones();
+				limiteInferior = medio + 1;
+			} else {
+				// datosEstadisticos.incrementarComparaciones();
+				limiteSuperior = medio - 1;
 			}
-			else if(elemento>datos[medio]) {
-				//datosEstadisticos.incrementarComparaciones();
-				limiteInferior = medio+1;
-			}
-			else {
-				//datosEstadisticos.incrementarComparaciones();
-				limiteSuperior = medio-1;
-			}
-			
-		}while(limiteSuperior>limiteInferior);
+
+			System.out.println("---------------------limiteSuperior " + limiteSuperior);
+			System.out.println("---------------------medio " + medio);
+			System.out.println("---------------------limiteInferior " + limiteInferior);
+
+		} while (limiteSuperior > limiteInferior);
+
+		datosEstadisticos.setDatoEncontrado(false);
+		datosEstadisticos.imprimirEstadisticos();
+		return datosEstadisticos;
+	}
+
+	public DatosEstadisticos busquedaBinaria(long dato, long vector[]) {
 		
+		int n = vector.length;
+		int centro, inf = 0, sup = n - 1;
+		
+		DatosEstadisticos datosEstadisticos = new DatosEstadisticos();
+		datosEstadisticos.setTamVector(n);
+
+		while (inf <= sup) {
+			centro = (sup + inf) / 2;
+			
+			datosEstadisticos.incrementarComparaciones();
+			
+			if (vector[centro] == dato) {
+				
+				datosEstadisticos.setDatoEncontrado(true);
+				datosEstadisticos.imprimirEstadisticos();
+				
+				return datosEstadisticos;
+				//return centro;
+			}
+			else if (dato < vector[centro]) {
+				sup = centro - 1;
+			} else {
+				inf = centro + 1;
+			}
+
+		}
+		
+		//return -1;
 		datosEstadisticos.setDatoEncontrado(false);
 		datosEstadisticos.imprimirEstadisticos();
 		return datosEstadisticos;
--- src/com/co/cbg/cp/ui/Menu.java
+++ src/com/co/cbg/cp/ui/Menu.java
@@ -67,7 +67,7 @@ public class Menu extends JFrame {
 	 */
 	public Menu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 580, 346);
+		setBounds(100, 100, 662, 346);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -173,7 +173,7 @@ public class Menu extends JFrame {
 		btnBuscarBinaria.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				DatosEstadisticos datosEstadisticos = busqueda.binaria( Long.parseLong(txtBuscar.getText().trim()), ordenamiento.getVectorBusquedaBinaria());
+				DatosEstadisticos datosEstadisticos = busqueda.busquedaBinaria( Long.parseLong(txtBuscar.getText().trim()), ordenamiento.getVectorBusquedaBinaria());
 				
 				txtTBinaria.setText(datosEstadisticos.getTimeElapsed().toString());
 				txtIBinaria.setText(datosEstadisticos.isDatoEncontrado() + "");
@@ -186,7 +186,7 @@ public class Menu extends JFrame {
 		contentPane.add(btnBuscarBinaria);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(137, 11, 417, 189);
+		panel.setBounds(137, 11, 499, 189);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -205,11 +205,11 @@ public class Menu extends JFrame {
 		panel.add(lblTiempoMilisegundos);
 		
 		JLabel lblNoComparaciones = new JLabel("Comparaciones");
-		lblNoComparaciones.setBounds(254, 33, 78, 14);
+		lblNoComparaciones.setBounds(268, 33, 78, 14);
 		panel.add(lblNoComparaciones);
 		
 		JLabel lblNoIntercambios = new JLabel(" Intercambios");
-		lblNoIntercambios.setBounds(342, 33, 65, 14);
+		lblNoIntercambios.setBounds(390, 33, 65, 14);
 		panel.add(lblNoIntercambios);
 		
 		JLabel lblMtodo = new JLabel("M\u00E9todo");
@@ -234,73 +234,73 @@ public class Menu extends JFrame {
 		
 		txtTBurbuja = new JTextField();
 		txtTBurbuja.setEditable(false);
-		txtTBurbuja.setBounds(139, 72, 97, 20);
+		txtTBurbuja.setBounds(139, 72, 119, 20);
 		panel.add(txtTBurbuja);
 		txtTBurbuja.setColumns(10);
 		
 		txtTSeleccion = new JTextField();
 		txtTSeleccion.setEditable(false);
-		txtTSeleccion.setBounds(139, 101, 97, 20);
+		txtTSeleccion.setBounds(139, 101, 119, 20);
 		panel.add(txtTSeleccion);
 		txtTSeleccion.setColumns(10);
 		
 		txtTHeap = new JTextField();
 		txtTHeap.setEditable(false);
-		txtTHeap.setBounds(139, 132, 97, 20);
+		txtTHeap.setBounds(139, 132, 119, 20);
 		panel.add(txtTHeap);
 		txtTHeap.setColumns(10);
 		
 		txtTMerge = new JTextField();
 		txtTMerge.setEditable(false);
-		txtTMerge.setBounds(139, 164, 97, 20);
+		txtTMerge.setBounds(139, 164, 119, 20);
 		panel.add(txtTMerge);
 		txtTMerge.setColumns(10);
 		
 		txtCBurbuja = new JTextField();
 		txtCBurbuja.setEditable(false);
-		txtCBurbuja.setBounds(254, 72, 78, 20);
+		txtCBurbuja.setBounds(268, 72, 112, 20);
 		panel.add(txtCBurbuja);
 		txtCBurbuja.setColumns(10);
 		
 		txtCSeleccion = new JTextField();
 		txtCSeleccion.setEditable(false);
-		txtCSeleccion.setBounds(254, 101, 78, 20);
+		txtCSeleccion.setBounds(268, 101, 112, 20);
 		panel.add(txtCSeleccion);
 		txtCSeleccion.setColumns(10);
 		
 		txtCHeap = new JTextField();
 		txtCHeap.setEditable(false);
-		txtCHeap.setBounds(254, 132, 78, 20);
+		txtCHeap.setBounds(268, 132, 112, 20);
 		panel.add(txtCHeap);
 		txtCHeap.setColumns(10);
 		
 		txtCMerge = new JTextField();
 		txtCMerge.setEditable(false);
-		txtCMerge.setBounds(254, 164, 78, 20);
+		txtCMerge.setBounds(268, 164, 112, 20);
 		panel.add(txtCMerge);
 		txtCMerge.setColumns(10);
 		
 		txtIBurbuja = new JTextField();
 		txtIBurbuja.setEditable(false);
-		txtIBurbuja.setBounds(342, 72, 65, 20);
+		txtIBurbuja.setBounds(390, 72, 99, 20);
 		panel.add(txtIBurbuja);
 		txtIBurbuja.setColumns(10);
 		
 		txtISeleccion = new JTextField();
 		txtISeleccion.setEditable(false);
-		txtISeleccion.setBounds(342, 101, 65, 20);
+		txtISeleccion.setBounds(390, 101, 99, 20);
 		panel.add(txtISeleccion);
 		txtISeleccion.setColumns(10);
 		
 		txtIHeap = new JTextField();
 		txtIHeap.setEditable(false);
-		txtIHeap.setBounds(342, 132, 65, 20);
+		txtIHeap.setBounds(390, 132, 99, 20);
 		panel.add(txtIHeap);
 		txtIHeap.setColumns(10);
 		
 		txtIMerge = new JTextField();
 		txtIMerge.setEditable(false);
-		txtIMerge.setBounds(342, 164, 65, 20);
+		txtIMerge.setBounds(390, 164, 99, 20);
 		panel.add(txtIMerge);
 		txtIMerge.setColumns(10);
 		
@@ -324,7 +324,7 @@ public class Menu extends JFrame {
 		panel.add(btnNuevoVector);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(137, 211, 417, 85);
+		panel_1.setBounds(137, 211, 499, 85);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -338,42 +338,42 @@ public class Menu extends JFrame {
 		
 		txtTSecuencial = new JTextField();
 		txtTSecuencial.setEditable(false);
-		txtTSecuencial.setBounds(141, 24, 86, 20);
+		txtTSecuencial.setBounds(141, 24, 110, 20);
 		panel_1.add(txtTSecuencial);
 		txtTSecuencial.setColumns(10);
 		
 		txtTBinaria = new JTextField();
 		txtTBinaria.setEditable(false);
-		txtTBinaria.setBounds(141, 54, 86, 20);
+		txtTBinaria.setBounds(141, 54, 110, 20);
 		panel_1.add(txtTBinaria);
 		txtTBinaria.setColumns(10);
 		
 		txtCSecuencial = new JTextField();
 		txtCSecuencial.setEditable(false);
-		txtCSecuencial.setBounds(253, 24, 79, 20);
+		txtCSecuencial.setBounds(272, 24, 102, 20);
 		panel_1.add(txtCSecuencial);
 		txtCSecuencial.setColumns(10);
 		
 		txtCBinaria = new JTextField();
 		txtCBinaria.setEditable(false);
-		txtCBinaria.setBounds(253, 54, 79, 20);
+		txtCBinaria.setBounds(272, 54, 102, 20);
 		panel_1.add(txtCBinaria);
 		txtCBinaria.setColumns(10);
 		
 		txtISecuencial = new JTextField();
 		txtISecuencial.setEditable(false);
-		txtISecuencial.setBounds(342, 24, 65, 20);
+		txtISecuencial.setBounds(384, 24, 65, 20);
 		panel_1.add(txtISecuencial);
 		txtISecuencial.setColumns(10);
 		
 		txtIBinaria = new JTextField();
 		txtIBinaria.setEditable(false);
-		txtIBinaria.setBounds(341, 54, 66, 20);
+		txtIBinaria.setBounds(383, 54, 66, 20);
 		panel_1.add(txtIBinaria);
 		txtIBinaria.setColumns(10);
 		
 		lblEncontrado_1 = new JLabel("Encontrado");
-		lblEncontrado_1.setBounds(342, 0, 65, 14);
+		lblEncontrado_1.setBounds(384, 0, 65, 14);
 		panel_1.add(lblEncontrado_1);
 		
 		txtBuscar = new JTextField();
